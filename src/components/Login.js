385f6493f15eeb4b29f1aeab8fe5da113463eb41
jana-almansour2 <jana.almansour2@gmail.com>
2026-01-31 00:11:11 +0300

    Added CSS for Login and Todo pages

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { signInWithGoogle } from "../services/authService";
+import "./Login.css"; 
 
 function Login() {
   const navigate = useNavigate();
@@ -15,10 +16,12 @@ function Login() {
   };
 
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Login Page</h1>
-      <button onClick={handleGoogleLogin}>
-        Login with Google
+    <div className="login-container">
+      <h1>Welcome to My Todo App</h1>
+      <button className="google-btn" onClick={handleGoogleLogin}>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" 
+             alt="Google logo" className="google-logo" />
+        Sign in with Google
       </button>
     </div>
   );
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,12 +2,12 @@ import React, { useEffect, useState } from "react";
 import { db, auth } from "../firebase";
 import { collection, addDoc, getDocs, query, where, deleteDoc, doc } from "firebase/firestore";
 import { logout } from "../services/authService";
+import "./Todo.css";
 
 function Todo() {
   const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState("");
 
-  // تجيب المهام الخاصة بالمستخدم
   const fetchTodos = async () => {
     const q = query(collection(db, "todos"), where("uid", "==", auth.currentUser.uid));
     const querySnapshot = await getDocs(q);
@@ -19,7 +19,6 @@ function Todo() {
     fetchTodos();
   }, []);
 
-  // إضافة مهمة جديدة
   const addTodo = async () => {
     if (newTodo.trim() === "") return;
     await addDoc(collection(db, "todos"), {
@@ -31,33 +30,32 @@ function Todo() {
     fetchTodos();
   };
 
-  // حذف مهمة
   const removeTodo = async (id) => {
     await deleteDoc(doc(db, "todos", id));
     fetchTodos();
   };
 
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
+    <div className="todo-container">
       <h1>My Todos</h1>
-      <div>
+      <div className="input-section">
         <input 
           type="text" 
           value={newTodo} 
           onChange={(e) => setNewTodo(e.target.value)} 
           placeholder="Add new todo" 
         />
-        <button onClick={addTodo}>Add</button>
+        <button className="add-btn" onClick={addTodo}>Add</button>
       </div>
-      <ul style={{ listStyle: "none", padding: 0 }}>
+      <ul className="todo-list">
         {todos.map(todo => (
-          <li key={todo.id}>
-            {todo.text} 
-            <button onClick={() => removeTodo(todo.id)}>Delete</button>
+          <li key={todo.id} className="todo-item">
+            {todo.text}
+            <button className="delete-btn" onClick={() => removeTodo(todo.id)}>Delete</button>
           </li>
         ))}
       </ul>
-      <button onClick={logout}>Logout</button>
+      <button className="logout-btn" onClick={logout}>Logout</button>
     </div>
   );
 }
